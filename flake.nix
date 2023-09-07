a2c5d93ce651d0c1907ab9cef7cbd45dca0aba0d
john bargman <darthpjb@gmail.com>
2023-09-07 16:20:24 +0100

    formatting pass

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     simple-nixos-mailserver.url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
   };
 
-  outputs = inputs@{ self, nixpkgs, agenix, nixinate, nixpkgs_unstable, simple-nixos-mailserver}: {
+  outputs = inputs@{ self, nixpkgs, agenix, nixinate, nixpkgs_unstable, simple-nixos-mailserver }: {
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
     apps.x86_64-linux = (inputs.nixinate.nixinate.x86_64-linux inputs.self).nixinate;
     nixosConfigurations = {
@@ -23,18 +23,18 @@
               domains = [ "mail.cybertrike.org" ];
               enable = true;
               # A list of all login accounts. To create the password hashes, use
-            # nix-shell -p mkpasswd --run 'mkpasswd -sm bcrypt'
-            loginAccounts = {
-            "John88@mail.cybertrike.org" = {
-                hashedPasswordFile = ./password.file;
-                aliases = ["postmaster@mail.cybertrike.org"];
-            };
-            };
+              # nix-shell -p mkpasswd --run 'mkpasswd -sm bcrypt'
+              loginAccounts = {
+                "John88@mail.cybertrike.org" = {
+                  hashedPasswordFile = ./password.file;
+                  aliases = [ "postmaster@mail.cybertrike.org" ];
+                };
+              };
 
-            # Use Let's Encrypt certificates. Note that this needs to set up a stripped
-            # down nginx and opens port 80.
-            certificateScheme = "acme-nginx";
-        };
+              # Use Let's Encrypt certificates. Note that this needs to set up a stripped
+              # down nginx and opens port 80.
+              certificateScheme = "acme-nginx";
+            };
           }
           agenix.nixosModules.default
           ./openstack.nix
@@ -43,9 +43,9 @@
             security.acme.acceptTerms = true;
             security.acme.defaults.email = "security@mail.cybertrike.org";
             environment.systemPackages = [
-                nixpkgs.legacyPackages.x86_64-linux.btop
-                nixpkgs.legacyPackages.x86_64-linux.tmux
-                nixpkgs.legacyPackages.x86_64-linux.neovim
+              nixpkgs.legacyPackages.x86_64-linux.btop
+              nixpkgs.legacyPackages.x86_64-linux.tmux
+              nixpkgs.legacyPackages.x86_64-linux.neovim
             ];
             imports = [
               "${nixpkgs}/nixos/modules/virtualisation/openstack-config.nix"
