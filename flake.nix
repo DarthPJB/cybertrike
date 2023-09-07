4b5b573a7e241982196dd75caa8f44aab3144b07
john bargman <darthpjb@gmail.com>
2023-09-07 16:19:59 +0100

    mailserver setup and https done

--- flake.nix
+++ flake.nix
@@ -19,14 +19,34 @@
           simple-nixos-mailserver.nixosModule
           {
             mailserver = {
+              fqdn = "mail.cybertrike.org";
+              domains = [ "mail.cybertrike.org" ];
               enable = true;
-              # ...
+              # A list of all login accounts. To create the password hashes, use
+            # nix-shell -p mkpasswd --run 'mkpasswd -sm bcrypt'
+            loginAccounts = {
+            "John88@mail.cybertrike.org" = {
+                hashedPasswordFile = ./password.file;
+                aliases = ["postmaster@mail.cybertrike.org"];
             };
+            };
+
+            # Use Let's Encrypt certificates. Note that this needs to set up a stripped
+            # down nginx and opens port 80.
+            certificateScheme = "acme-nginx";
+        };
           }
           agenix.nixosModules.default
           ./openstack.nix
           ./darthpjb.nix
           {
+            security.acme.acceptTerms = true;
+            security.acme.defaults.email = "security@mail.cybertrike.org";
+            environment.systemPackages = [
+                nixpkgs.legacyPackages.x86_64-linux.btop
+                nixpkgs.legacyPackages.x86_64-linux.tmux
+                nixpkgs.legacyPackages.x86_64-linux.neovim
+            ];
             imports = [
               "${nixpkgs}/nixos/modules/virtualisation/openstack-config.nix"
             ];
