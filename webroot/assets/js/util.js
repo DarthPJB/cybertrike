6070a3d278ca136cf99372670f2382eb3a483d44
john bargman <darthpjb@gmail.com>
2023-09-09 16:30:06 +0100

    updated and polished

--- webroot/assets/js/util.js
+++ webroot/assets/js/util.js
@@ -1,3 +1,13 @@
+(function () {
+    var body = document.body,
+            e = document.documentElement,
+            scrollPercent;
+    $(window).unbind("scroll").scroll(function () {
+        scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
+        body.style.backgroundPosition = "0px " + scrollPercent + "%";
+    });
+    })();
+
 function scaleToFill() {
     $('video.video-background').each(function(index, videoTag) {
        var $video = $(videoTag),
