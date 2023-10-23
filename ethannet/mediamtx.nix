f87695b34dab02834f67e1d36223e0df65d0dc60
l33 <lee.hughes@platonic.systems>
2023-10-23 23:25:50 +0100

    added new keys for youtube and kick

--- ethannet/mediamtx.nix
+++ ethannet/mediamtx.nix
@@ -6,8 +6,14 @@
 
   services.mediamtx =
     let
-      stream_server = "lhr03.contribute.live-video.net";
-      stream_key = "live_903856572_iUoDqW2G7htcCJCsjqeuNKKa5ccGRy";
+      stream_server_twitch = "lhr03.contribute.live-video.net";
+      stream_key_twitch = "live_903856572_iUoDqW2G7htcCJCsjqeuNKKa5ccGRy";
+
+      stream_server_youtube = "a.rtmp.youtube.com/live2";
+      stream_key_youtube = "xuta-15b1-m92p-u41z-fxc0";
+
+      stream_server_kick = "fa723fc1b171.global-contribute.live-video.net";
+      stream_key_kick = "sk_us-west-2_FObEyLcKaSLi_RWZdhRhagoTPdjByAUWrY3L9MhOGUu";
     in
     {
       enable = true;
@@ -22,7 +28,7 @@
             restream = {
               runOnInitRestart = true;
               runOnReady = ''
-                ${pkgs.ffmpeg}/bin/ffmpeg -i rtmp://localhost/restream -c copy -f flv rtmp://${stream_server}/app/${stream_key}'';
+                ${pkgs.ffmpeg}/bin/ffmpeg -i rtmp://localhost/restream -c copy -f flv rtmp://${stream_server_twitch}/app/${stream_key_twitch} -c copy -f flv rtmp://${stream_server_kick}/app/${stream_key_kick}'';
               runOnReadyRestart = true;
             };
           };
