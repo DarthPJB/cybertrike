a7fb3487eb142a224e06ad1d232d992aa24fe4e8
john bargman <darthpjb@gmail.com>
2023-10-24 03:56:40 +0100

    fixed everything fuck instagram

--- ethannet/mediamtx.nix
+++ ethannet/mediamtx.nix
@@ -6,14 +6,34 @@
 
   services.mediamtx =
     let
-      tw_stream_server = "lhr03.contribute.live-video.net";
+      tw_stream_server = "lhr03.contribute.live-video.net/app";
       tw_stream_key = "live_903856572_iUoDqW2G7htcCJCsjqeuNKKa5ccGRy";
 
       yt_stream_server = "a.rtmp.youtube.com/live2";
       yt_stream_key = "xuta-15b1-m92p-u41z-fxc0";
 
-      ki_stream_server = "fa723fc1b171.global-contribute.live-video.net";
+      ki_stream_server = "fa723fc1b171.global-contribute.live-video.net/app";
       ki_stream_key = "sk_us-west-2_FObEyLcKaSLi_RWZdhRhagoTPdjByAUWrY3L9MhOGUu";
+
+      fb_stream_server = "live-api-s.facebook.com:443/rtmp";
+      fb_stream_key = "FB-114005818335608-0-Abw335iVypxJ4xCA";
+      restream_script = pkgs.writeShellScriptBin "restream.sh"
+        ''              
+            ${pkgs.ffmpeg}/bin/ffmpeg -i rtmp://localhost/restream \
+            -tune zerolatency -map v:0 -map a:0 -c copy -f tee \
+            "[f=flv:onfail=ignore]rtmp://${tw_stream_server}/${tw_stream_key}|\
+            [f=flv:onfail=ignore]rtmp://${yt_stream_server}/${yt_stream_key}|\
+            [f=flv:onfail=ignore]rtmps://${ki_stream_server}/${ki_stream_key}" &
+
+            ${pkgs.ffmpeg}/bin/ffmpeg -i rtmp://localhost/restream \
+            -tune zerolatency -f flv -maxrate 4500k -preset ultrafast -g 48 -keyint_min 48 -sc_threshold 0 \
+            -vf "crop=9/16*ih:ih" -strict -2 -ac 1 -ar 44100 -b:a 96k -pix_fmt yuv420p \
+            -map v:0 -map 0:a -c:a copy -c:v libx264 -f tee \
+            "[f=flv:onfail=ignore]rtmps://${fb_stream_server}/${fb_stream_key}" &
+
+            wait
+
+        '';
     in
     {
       enable = true;
@@ -27,16 +47,10 @@
           paths = {
             restream = {
               runOnInitRestart = true;
-#                ${pkgs.ffmpeg}/bin/ffmpeg -i rtmp://localhost/restream \
-#                -filter_complex "[0:v]crop=9/16*ih:ih[vPhone]"
-#                -c:v libx264 -map 0:a -map "[vPhone]" -f tee \
-#                "[f=flv:onfail=ignore]rtmp://${tw_stream_server}/app/${tw_stream_key}"&;
-#                |\
-              runOnReady = ''              
-                ${pkgs.ffmpeg}/bin/ffmpeg -i rtmp://localhost/restream -c copy -map 0:v -map 0:a -f tee \
-                "[f=flv:onfail=ignore]rtmp://${tw_stream_server}/app/${tw_stream_key}|[f=flv:onfail=ignore]rtmps://${ki_stream_server}/app/${ki_stream_key}|[f=flv:onfail=ignore]rtmp://${yt_stream_server}/app/${yt_stream_key}"'';
+              runOnReady = "${restream_script}/bin/restream.sh";
               runOnReadyRestart = true;
             };
+
           };
         };
     };
--- readme.md
+++ readme.md
@@ -4,4 +4,6 @@ All rights reserved, Lee Hughes and John bargman - 2023
 
 
 ## test-stream command with ffmpeg
-``` ffmpeg -re -f lavfi -i testsrc2=size=960x540 -f lavfi -i aevalsrc="sin(0*2*PI*t)" -vcodec libx264 -r 30 -g 30 -preset fast -vb 3000k -pix_fmt rgb24 -pix_fmt yuv420p -f flv rtmp://149.5.115.135/restream ```
+``` bash
+ffmpeg -re -f lavfi -i testsrc2=size=960x540 -f lavfi -i aevalsrc="sin(0*2*PI*t)" -acodec aac -vcodec libx264 -r 30 -g 30 -preset fast -vb 4500k -pix_fmt yuv420p -f flv rtmp://149.5.115.135/restream
+```
